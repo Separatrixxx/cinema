1690abd7cb6a6f6c3198bf74e0701ad1cbfc8d11
Separatrix <87654656+Separatrixxx@users.noreply.github.com>
2022-07-04 18:33:47 +0300

    Add files via upload

--- src/components/FilmForm4.js
+++ src/components/FilmForm4.js
@@ -4,27 +4,27 @@ import {Link} from "react-router-dom";
 
 function FilmForm4() {
     return(
-        <div id="horizontal-scroller-3" className="flex overflow-x-hidden mt-10 mx-10">
+        <div id="horizontal-scroller-3" className="flex overflow-x-auto md:overflow-x-hidden mt-10 mx-10">
             <Link to="/movie">
-                <div className="w-64 h-96 bg-red-200 mx-5 rounded-xl hover:bg-red-300 ease-in-out duration-300 cursor-pointer"></div>
+                <div className="w-40 md:w-72 h-64 md:h-96 bg-red-200 mx-5 rounded-xl hover:bg-red-300 ease-in-out duration-300 cursor-pointer"></div>
             </Link>
             <Link to="/movie">
-                <div className="w-72 h-96 bg-orange-200 mx-5 rounded-xl hover:bg-orange-300 ease-in-out duration-300 cursor-pointer"></div>
+                <div className="w-40 md:w-72 h-64 md:h-96 bg-orange-200 mx-5 rounded-xl hover:bg-orange-300 ease-in-out duration-300 cursor-pointer"></div>
             </Link>
             <Link to="/movie">
-                <div className="w-72 h-96 bg-yellow-200 mx-5 rounded-xl hover:bg-yellow-300 ease-in-out duration-300 cursor-pointer"></div>
+                <div className="w-40 md:w-72 h-64 md:h-96 bg-yellow-200 mx-5 rounded-xl hover:bg-yellow-300 ease-in-out duration-300 cursor-pointer"></div>
             </Link>
             <Link to="/movie">
-                <div className="w-72 h-96 bg-green-200 mx-5 rounded-xl hover:bg-green-300 ease-in-out duration-300 cursor-pointer"></div>
+                <div className="w-40 md:w-72 h-64 md:h-96 bg-green-200 mx-5 rounded-xl hover:bg-green-300 ease-in-out duration-300 cursor-pointer"></div>
             </Link>
             <Link to="/movie">
-                <div className="w-72 h-96 bg-blue-200 mx-5 rounded-xl hover:bg-blue-300 ease-in-out duration-300 cursor-pointer"></div>
+                <div className="w-40 md:w-72 h-64 md:h-96 bg-blue-200 mx-5 rounded-xl hover:bg-blue-300 ease-in-out duration-300 cursor-pointer"></div>
             </Link>
             <Link to="/movie">
-                <div className="w-72 h-96 bg-purple-200 mx-5 rounded-xl hover:bg-purple-300 ease-in-out duration-300 cursor-pointer"></div>
+                <div className="w-40 md:w-72 h-64 md:h-96 bg-purple-200 mx-5 rounded-xl hover:bg-purple-300 ease-in-out duration-300 cursor-pointer"></div>
             </Link>
             <Link to="/movie">
-                <div className="w-72 h-96 bg-pink-200 mx-5 rounded-xl hover:bg-pink-300 ease-in-out duration-300 cursor-pointer"></div>
+                <div className="w-40 md:w-72 h-64 md:h-96 bg-pink-200 mx-5 rounded-xl hover:bg-pink-300 ease-in-out duration-300 cursor-pointer"></div>
             </Link>
         </div>
     );

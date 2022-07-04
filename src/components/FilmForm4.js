80971aeb66b4b7b9936cc117309767179f564843
Separatrix <87654656+Separatrixxx@users.noreply.github.com>
2022-07-04 18:47:30 +0300

    Add files via upload

--- src/components/FilmForm4.js
+++ src/components/FilmForm4.js
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom";
 
 function FilmForm4() {
     return(
-        <div id="horizontal-scroller-1" className="flex overflow-x-auto md:overflow-x-hidden mt-10 mx-6 md:mx-10">
+        <div id="horizontal-scroller-1" className="flex overflow-x-auto md:overflow-x-hidden mt-5 md:mt-10 mx-6 md:mx-10">
             <Link to="/movie">
                 <div className="w-24 md:w-64 h-36 md:h-96 bg-red-200 mx-2 md:mx-5 rounded-xl hover:bg-red-300 ease-in-out duration-300 cursor-pointer"></div>
             </Link>

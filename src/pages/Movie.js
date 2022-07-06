a764728be7c3a18920f03a8249a8fcfe7504ec38
Separatrix <87654656+Separatrixxx@users.noreply.github.com>
2022-07-06 21:30:14 +0300

    Update Movie.js

--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -2,54 +2,8 @@ import React, {Component} from 'react';
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
-class Movie extends Component {
-
-    state = {
-        torrentInfoHash: "",
-        torrentMagnetURI: "",
-        torrentName: "",
-        torrentProgress: "",
-        torrentFiles: []
-    }
-
-    componentDidMount() {
-        // Sintel, a free, Creative Commons movie
-        const torrentId = 'magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent';
-
-        const WebTorrent = require('webtorrent');
-        const client = new WebTorrent();
-
-        client.on('error', err => {
-            console.log('[+] Webtorrent error: ' + err.message);
-        });
-
-        client.add(torrentId, (torrent) => {
-            const interval = setInterval(() => {
-                // console.log('[+] Progress: ' + (torrent.progress * 100).toFixed(1) + '%')
-                this.setState({torrentProgress: (torrent.progress * 100).toFixed(1) + '%'});
-            }, 5000);
-            torrent.on('done', () => {
-                console.log('Progress: 100%');
-                clearInterval(interval);
-            })
-
-            this.setState({
-                torrentInfoHash: torrent.infoHash,
-                torrentMagnetURI: torrent.magnetURI,
-                torrentName: torrent.name,
-                torrentFiles: torrent.files
-            });
-
-            // TODO Figure out a better way to render these files
-            this.state.torrentFiles.map((file, i) => {
-                file.appendTo('body');
-            })
-
-        });
-    }
-
-    render() {
-        return (
+function Movie () {
+    return (
             <div className="scroll-smooth bg-neutral-900">
                 <Header />
                 <div className="flex flex-col md:flex-row w-full py-28 md:py-20 px-14 md:px-32">
@@ -79,15 +33,9 @@ class Movie extends Component {
                     </div>
                 </div>
                 <div id="kinoplayertop" data-player="bazon" data-kinopoisk="9691"></div>
-                <div>
-                    <h1>{this.state.torrentName}</h1>
-                    <p><b>Torrent Info Hash: </b>{this.state.torrentInfoHash}</p>
-                    <p><b>Torrent Progress: </b>{this.state.torrentProgress}</p>
-                </div>
                 <Footer />
             </div>
         );
-    }
 }
 
 

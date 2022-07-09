0fad8a2965ad44e19b2e1f42b70612b73bc729c1
Separatrix <87654656+Separatrixxx@users.noreply.github.com>
2022-07-09 21:49:35 +0300

    Add files via upload

--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from '../components/Header'
+import HeaderFilm from '../components/HeaderFilm'
 import Footer from '../components/Footer'
 import MovieForm from "../components/MovieForm";
 
@@ -10,7 +10,7 @@ function Movie () {
 
     return (
             <div className="scroll-smooth bg-neutral-900">
-                <Header />
+                <HeaderFilm />
                 <MovieForm />
                 <div id="kinoplayertop" data-player="bazon" data-kinopoisk={kinopoiskId}></div>
                 <Footer />

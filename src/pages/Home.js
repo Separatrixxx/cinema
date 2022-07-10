fb1169c5da48c597f3033b0855bbb28aa486186e
Separatrix <87654656+Separatrixxx@users.noreply.github.com>
2022-07-11 02:43:10 +0300

    Add files via upload

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import FilmForm1 from '../components/FilmForm1'
 import FilmForm2 from '../components/FilmForm2'
 import FilmForm3 from '../components/FilmForm3'
 
@@ -9,9 +10,10 @@ function Home () {
     return (
         <div className="scroll-smooth bg-neutral-900">
             <Header />
-            <h1 className="text-white font-bold text-2xl md:text-4xl pt-20 pl-10 md:pl-44">Жанры</h1>
+            <FilmForm1 />
+            <h1 className="text-white font-bold text-2xl md:text-4xl pt-5 md:pt-12 pl-10 md:pl-44">Жанры</h1>
             <FilmForm3 />
-            <h1 className="text-white font-bold text-2xl md:text-4xl pt-12 pl-10 md:pl-44">Все фильмы и сериалы</h1>
+            <h1 className="text-white font-bold text-2xl md:text-4xl pt-5 md:pt-12 pl-10 md:pl-44">Все фильмы и сериалы</h1>
             <FilmForm2 />
             <Footer />
         </div>
